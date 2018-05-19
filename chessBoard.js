9c1e1343a5707d584cc6b15bc146405c78f7099e
zeebear <zeebear@users.noreply.github.com>
2018-05-19 02:15:15 -0300

    chessBoard up and running

---
+++ chessBoard.js
@@ -0,0 +1,18 @@
+let size = 8;
+let rowLength = size + 1;
+let boardArea = size * rowLength;
+let output = "";
+
+for (let counter = 1; counter < boardArea; counter++) {
+  if (counter % rowLength == 0) {
+    output += "\n";
+  }
+  else if (counter % 2 == 1) {
+    output += " ";
+  }
+  else if (counter % 2 == 0) {
+    output += "#";
+  }
+}
+
+console.log(output);
